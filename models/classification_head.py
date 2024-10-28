ed14b56cfaef530df5f3448ceed50099b53faef3
RaffeLegend <oldgirafferiver@gmail.com>
2024-10-28 20:02:34 +0800

    [#1] fix: remove classification name

--- models/classification_head.py
+++ models/classification_head.py
@@ -8,9 +8,9 @@ CHANNELS = {
 }
 
 class Classification(nn.Module):
-    def __init__(self, name, num_classes=1):
+    def __init__(self, num_classes=1):
         super(Classification, self).__init__()
-        self.fc = nn.Linear( CHANNELS[name], num_classes )
+        self.fc = nn.Linear( CHANNELS["RN50"], num_classes )
         # self.model = resnet50(pretrained=True)
 
     def forward(self, x, return_feature=False):
