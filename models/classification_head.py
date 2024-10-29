454c51d888f241cbe8396bf61d41a2b2060a002d
RaffeLegend <oldgirafferiver@gmail.com>
2024-10-29 15:22:05 +0800

    [#1] debug: debug fc

--- models/classification_head.py
+++ models/classification_head.py
@@ -18,9 +18,12 @@ class Classification(nn.Module):
 
     def forward(self, x, return_feature=False):
         print(x[3].shape)
-        x = x[3].view(2, -1).transpose(1, 2)
+        x = x[3].view(256, -1)
         print(x.shape)
         x = self.relu(self.fc1(x))
+        print(x.shape)
         x = self.relu(self.fc2(x))
+        print(x.shape)
         x = self.fc(x)
+        print(x.shape)
         return x
