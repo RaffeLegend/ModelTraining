0c1615d5be1c0c3072a339eb28e5f6bbe317fa0c
RaffeLegend <oldgirafferiver@gmail.com>
2024-11-01 01:22:47 +0800

    [#1] func: rebuilt the fc layer

--- models/classification_head.py
+++ models/classification_head.py
@@ -10,15 +10,15 @@ CHANNELS = {
 class Classification(nn.Module):
     def __init__(self, num_classes=1):
         super(Classification, self).__init__()
-        self.fc1 = nn.Linear(2048 * 7 *7 , 1024)
-        self.fc2 = nn.Linear(1024, 256)
-        self.fc = nn.Linear(256, num_classes)
-        self.relu = nn.ReLU()
-        # self.model = resnet50(pretrained=True)
+        
+        self.fc_layer = nn.Sequential(
+            nn.Linear(2048 * 7 * 7, 1024),  
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(1024, num_classes)
+        )
 
     def forward(self, x, return_feature=False):
         x = x[3].view((x[3].shape)[0], -1)
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        x = self.fc(x)
+        x = self.fc_layer(x)
         return x
