37aabf5230293b8f101e32dc35718e45b1a44b60
RaffeLegend <oldgirafferiver@gmail.com>
2024-11-10 18:25:30 +0000

    [debug] remove func in data augmentation

--- data/datasets.py
+++ data/datasets.py
@@ -96,15 +96,19 @@ class RealFakeDataset(Dataset):
 
         print("mean and std stats are from: ", stat_from)
         # if '2b' not in opt.arch:
-        if False:
+        if True:
             print ("using Official CLIP's normalization")
+            # self.transform = transforms.Compose([
+            #     rz_func,
+            #     transforms.Lambda(lambda img: data_augment(img, opt)),
+            #     crop_func,
+            #     flip_func,
+            #     transforms.ToTensor(),
+            #     transforms.Normalize( mean=MEAN[stat_from], std=STD[stat_from] ),
+            # ])
             self.transform = transforms.Compose([
-                rz_func,
-                transforms.Lambda(lambda img: data_augment(img, opt)),
-                crop_func,
-                flip_func,
                 transforms.ToTensor(),
-                transforms.Normalize( mean=MEAN[stat_from], std=STD[stat_from] ),
+                transforms.Normalize(mean=MEAN[stat_from], std=STD[stat_from]),
             ])
         else:
             print ("Using CLIP 2B transform")
